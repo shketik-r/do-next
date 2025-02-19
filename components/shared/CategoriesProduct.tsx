d8a9539cd7685ccf5461c1284fa482e95731dc9a
Shketik_R <ruslans3101@gmail.com>
2025-02-19 23:40:06 +0300

    in 2

--- components/shared/CategoriesProduct.tsx
+++ components/shared/CategoriesProduct.tsx
@@ -1,4 +1,4 @@
-// import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 import React from 'react';
 
 interface Props {
@@ -11,22 +11,20 @@ interface Props {
 export const CategoriesProduct: React.FC<Props> = ({ className  }) => {
   return (
 
-    <>
-    </>
 
-    // <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
-    //   {
-    //     cats.map((cat:string, index) => {
-    //       return (
-    //         <button
-    //           className={cn('flex items-center font-bold h-11 rounded-2xl px-5', index === activeIndex && 'bg-white shadow-md shadow-gray-200 text-primary')}
-    //           key={index}>
-    //           <span>{cat}</span>
-    //         </button>
-    //       )
-    //     })
-    //   }
+    <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
+      {/* {
+        cats.map((cat:string, index) => {
+          return (
+            <button
+              className={cn('flex items-center font-bold h-11 rounded-2xl px-5', index === activeIndex && 'bg-white shadow-md shadow-gray-200 text-primary')}
+              key={index}>
+              <span>{cat}</span>
+            </button>
+          )
+        })
+      } */}
 
-    // </div>
+    </div>
   );
 };
